917b4617737229ad32c1bac4076c4cb9ef0ae235
Divyajyoti <Divyajyoti182000@gmail.com>
2023-10-16 15:40:53 +0530

    LMS : Image-Form Completed and Tested

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import { FileUpload } from "@/components/file-upload";
-import { Button } from "@/components/ui/button";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { Course } from "@prisma/client";
 import axios from "axios";
 import { ImageIcon, Pencil, PlusCircle } from "lucide-react";
@@ -12,13 +9,18 @@ import { useState } from "react";
 import toast from "react-hot-toast";
 import * as z from "zod";
 
+import { FileUpload } from "@/components/file-upload";
+import { Button } from "@/components/ui/button";
+
 interface ImageFormProps {
   initialData: Course;
   courseId: string;
 }
 
 const formSchema = z.object({
-  imageUrl: z.string().min(1, { message: "Image is required" }),
+  imageUrl: z.string().min(1, {
+    message: "Image is required",
+  }),
 });
 
 export const ImageForm = ({ initialData, courseId }: ImageFormProps) => {
@@ -34,15 +36,15 @@ export const ImageForm = ({ initialData, courseId }: ImageFormProps) => {
       toast.success("Course updated");
       toggleEdit();
       router.refresh();
-    } catch (error) {
-      toast.error("Something went wrong!");
+    } catch {
+      toast.error("Something went wrong");
     }
   };
 
   return (
     <div className="mt-6 border bg-slate-100 rounded-md p-4">
       <div className="font-medium flex items-center justify-between">
-        Course Image
+        Course image
         <Button onClick={toggleEdit} variant="ghost">
           {isEditing && <>Cancel</>}
           {!isEditing && !initialData.imageUrl && (
@@ -53,26 +55,27 @@ export const ImageForm = ({ initialData, courseId }: ImageFormProps) => {
           )}
           {!isEditing && initialData.imageUrl && (
             <>
-              <Pencil className="h-4 2-4 mr-2" />
+              <Pencil className="h-4 w-4 mr-2" />
               Edit image
             </>
           )}
         </Button>
       </div>
-      {!isEditing && !initialData.imageUrl ? (
-        <div className="flex items-center justify-center h-60 bg-slate-200 rounded-md">
-          <ImageIcon className="h-10 w-10 text-slate-500" />
-        </div>
-      ) : (
-        <div className="relative aspect-video mt-2">
-          <Image
-            alt="Upload"
-            fill
-            className="object-cover rounded-md"
-            src={initialData.imageUrl || ""}
-          />
-        </div>
-      )}
+      {!isEditing &&
+        (!initialData.imageUrl ? (
+          <div className="flex items-center justify-center h-60 bg-slate-200 rounded-md">
+            <ImageIcon className="h-10 w-10 text-slate-500" />
+          </div>
+        ) : (
+          <div className="relative aspect-video mt-2">
+            <Image
+              alt="Upload"
+              fill
+              className="object-cover rounded-md"
+              src={initialData.imageUrl}
+            />
+          </div>
+        ))}
       {isEditing && (
         <div>
           <FileUpload
