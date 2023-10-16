c50522a4a91f0a2e8908f16e33470dbb27496361
Divyajyoti <Divyajyoti182000@gmail.com>
2023-10-16 13:00:10 +0530

    LMS : Day-3 Part-1

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,6 +1,11 @@
+import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
+import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
+import { DescriptionForm } from "./_components/description-form";
+import { ImageForm } from "./_components/image-form";
+import { TitleForm } from "./_components/title-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -44,8 +49,12 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
         <div className="flex items-center gap-x-2">
+          <IconBadge icon={LayoutDashboard} />
           <h2 className="text-xl"> Customize your course</h2>
         </div>
+        <TitleForm initialData={course} courseId={course.id} />
+        <DescriptionForm initialData={course} courseId={course.id} />
+        <ImageForm initialData={course} courseId={course.id} />
       </div>
     </div>
   );
