f249283a55657e32c8601710732ef0eb1e1cdbf2
Divyajyoti <Divyajyoti182000@gmail.com>
2023-10-23 21:08:01 +0530

    LMS : Working on Chapters

---
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -0,0 +1,17 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const ChapterIdPage = async ({
+  params,
+}: {
+  params: { courseId: string; chapterId: string };
+}) => {
+  const { userId } = auth();
+  if (!userId) {
+    return redirect("/");
+  }
+  return <div></div>;
+};
+
+export default ChapterIdPage;
--- app/(course)/courses/[courseId]/page.tsx
+++ app/(course)/courses/[courseId]/page.tsx
@@ -1,5 +1,27 @@
-const CoursePageId = () => {
-  return <div>Course Page Id</div>;
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+const CoursePageId = async ({ params }: { params: { courseId: string } }) => {
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+    include: {
+      chapters: {
+        where: {
+          isPublished: true,
+        },
+        orderBy: {
+          position: "asc",
+        },
+      },
+    },
+  });
+
+  if (!course) {
+    return redirect("/");
+  }
+  return redirect(`/courses/${course.id}/chapters/${course.chapters[0].id}`);
 };
 
 export default CoursePageId;
